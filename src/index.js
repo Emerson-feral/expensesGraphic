3a078647868bea67d8eb5df0a83e25c76711f341
emerson-feral <emerson_feral@hotmail.com>
2025-04-02 22:09:53 -0300

    Graph source

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const csvParser = require("csv-parser");
+
+const filePath = process.env.PATHCSV;
+
+const categories = {
+  Alimentação: ["Grao de Ouro", "Finesse Salgados"],
+  Contas: ["Pg *Ton Alessandra F", "Conta Vivo"],
+};
+
+const costCategories = ({ title }) => {
+  for (const element in categories) {
+    if (categories[element].some((keyword) => title.includes(keyword))) {
+      return element;
+    }
+  }
+  return "Others";
+};
+
+const processCSV = () => {
+  const costs = {};
+
+  fs.createReadStream(filePath)
+    .pipe(csvParser())
+    .on("data", (row) => {
+      const category = costCategories(row);
+      const value = parseFloat(row.amount) || 0;
+
+      if (!costs[category]) {
+        costs[category] = 0;
+      }
+      costs[category] += value;
+    })
+    .on("end", () => {
+      fs.writeFileSync("costs.json", JSON.stringify(costs, null, 2));
+      console.log("File costs.json generated");
+    });
+};
+
+processCSV();
